eb407d3201a83619b7546821d774c560e6e3a0ff
ian038 <ianphua3@gmail.com>
2022-05-12 17:55:01 -0400

    Deployed smart contract to rinkeby and initialized frontend

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -3,4 +3,10 @@ require('dotenv').config()
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_NETWORK,
+      accounts: [process.env.ACCOUNT]
+    }
+  }
 };
